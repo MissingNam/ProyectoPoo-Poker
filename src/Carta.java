1a1cb4abe1f988b5ad05854eb275cf22749d6ae1
MissigNam <jhared.martinez@uabc.edu.mx>
2025-05-06 20:53:03 -0700

    Añadido Mazo y Jugador

--- src/Carta.java
+++ src/Carta.java
@@ -1,18 +1,24 @@
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.event.*;
 
 
 public class Carta extends JButton {
 
-    int Categoria;
-    String Palo;
-    public Carta(int Categoria, String Palo)
+    int categoria;
+    String palo;
+    ImageIcon imagen;
+    ImageIcon reverso = new ImageIcon("imagenes\\fila-1-columna-1.png");
+    String ubicacion;
+    boolean visible = false;
+
+    public Carta(int categoria, String palo)
     {
-        this.Categoria = Categoria;
-        this.Palo = Palo;
+        this.categoria = categoria;
+        this.palo = palo;
         // colocarle la categoria
-        int columna = Categoria;
+        int columna = categoria;
         if(columna == 1)
         {
             columna = 14;
@@ -20,7 +26,7 @@ public class Carta extends JButton {
 
         //colocarle el palo
         int fila = 1;
-        switch(Palo)
+        switch(palo)
         {
             case "Trebol":
                 fila = 1;
@@ -39,18 +45,48 @@ public class Carta extends JButton {
                 break;
         }
                
-        String ubicacion = ("imagenes\\fila-"+fila+"-columna-"+columna+".png");
-        ImageIcon imagen = new ImageIcon(ubicacion);
+        ubicacion = ("imagenes\\fila-"+fila+"-columna-"+columna+".png");
+        imagen = new ImageIcon(ubicacion);
 
-        this.setIcon(imagen);
+        this.setIcon(reverso);
         this.setSize(59,89);
-            
 
         
 
+
+    }
+    
+    @Override
+        protected void processMouseEvent(MouseEvent e)
+        {
+            if(e.getID() == MouseEvent.MOUSE_PRESSED)
+            {
+                voltear();
+            }
+            super.processMouseEvent(e);
+        }
+
+
+    public String getPalo()
+    {
+        return palo;
     }
 
+    public int getCategoria()
+    {
+        return categoria;
+    }
 
+    public void voltear()
+    {
+        visible = !visible;
+        if(visible)
+        {
+            this.setIcon(imagen);
+        } else {
+            this.setIcon(reverso);
+        }
 
+    }
 
 }
